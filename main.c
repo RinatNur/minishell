147c199aec56c8c7da4a81ba882656c96aa45411
Rinat <nuretdin1405@gmail.com>
2020-12-09 16:46:09 +0300

    main header

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "minishell.h"
+
+
+int     main(int ac, char **av, char **envp)
+{
+    t_data      data;
+	t_list  	*list;
+
+    return(0);
+}
---
+++ minishell.h
@@ -0,0 +1,17 @@
+#ifndef MiNISHELL_H
+# define MiNISHELL_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <./libft/libft.h>
+#include <dirent.h>
+#include "processing.h"
+#include "parser.h"
+
+
+#endif
