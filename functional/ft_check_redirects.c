fb364fa77f0dea2dbac1955f8265c8142d3a6486
Rinat <nuretdin1405@gmail.com>
2021-01-12 17:33:47 +0300

    norm ft_check_redirects.c

--- functional/ft_check_redirects.c
+++ functional/ft_check_redirects.c
@@ -1,55 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_redirects.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jheat <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/12 17:00:05 by jheat             #+#    #+#             */
+/*   Updated: 2021/01/12 17:00:10 by jheat            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "processing.h"
 
-int		ft_check_redirects(t_data *data, t_list *command)
+static void			init_values(t_data *data)
 {
-	int 	file;
-	DIR    *check_if_dir;
 	data->wr_file_name = NULL;
 	data->rd_file_name = NULL;
 	data->redir_pipe_flag = 0;
 	data->redir_flag = 0;
+}
 
+static int			redirect_in_simple_and_rewrite(t_data *data)
+{
+	int		file;
+	DIR		*check_if_dir;
+
+	check_if_dir = opendir(REDIR->filename);
+	file = open(REDIR->filename, O_WRONLY | O_CREAT, 0666);
+	if (check_if_dir)
+	{
+		ft_error_print(MSHELL, REDIR->filename, NULL, ERR3);
+		data->redir_flag = 1;
+		return (1);
+	}
+	if (file == -1)
+	{
+		ft_error_print(MSHELL, REDIR->filename, NULL, ERR5);
+		data->redir_flag = 1;
+		return (1);
+	}
+	data->wr_file_name = REDIR->filename;
+	data->wr_type_redir = REDIR->redirect_type;
+	close(file);
+	return (0);
+}
+
+static int			redirect_from(t_data *data)
+{
+	int		file;
+	DIR		*check_if_dir;
+
+	check_if_dir = opendir(REDIR->filename);
+	file = open(REDIR->filename, O_RDONLY, 0666);
+	if (check_if_dir)
+	{
+		ft_error_print(NULL, data->ar[0], "stdin", ERR3);
+		data->redir_flag = 1;
+		return (1);
+	}
+	if (file == -1)
+	{
+		(errno == 2) ? ft_error_print(MSHELL, REDIR->filename, NULL, ERR1)
+		: ft_error_print(MSHELL, REDIR->filename, NULL, ERR5);
+		data->redir_pipe_flag = 1;
+		return (1);
+	}
+	data->rd_file_name = REDIR->filename;
+	close(file);
+	return (0);
+}
+
+int					ft_check_redirects(t_data *data, t_list *command)
+{
+	init_values(data);
 	if (!data->redirect_list)
 		return (1);
 	while (data->redirect_list)
 	{
-		if (REDIR->redirect_type == into_file || REDIR->redirect_type == into_file_with_rewrite)
+		if (REDIR->redirect_type == into_file
+			|| REDIR->redirect_type == into_file_with_rewrite)
 		{
-			if ((check_if_dir = opendir(REDIR->filename)))
-			{
-				ft_error_print(MSHELL, REDIR->filename, NULL, ERR3);
-				data->redir_flag = 1;
+			if ((redirect_in_simple_and_rewrite(data)) == 1)
 				return (1);
-			}
-			if ((file = open(REDIR->filename, O_WRONLY | O_CREAT, 0666)) == -1)
-			{
-				ft_error_print(MSHELL, REDIR->filename, NULL, ERR5);
-				data->redir_flag = 1;
-				return (1);
-			}
-			data->wr_file_name = REDIR->filename;
-			data->wr_type_redir = REDIR->redirect_type;
 		}
 		else
 		{
-			if ((check_if_dir = opendir(REDIR->filename)))
-			{
-				ft_error_print(NULL,data->ar[0], "stdin", ERR3);
-				data->redir_flag = 1;
+			if ((redirect_from(data)) == 1)
 				return (1);
-			}
-			if ((file = open(REDIR->filename, O_RDONLY, 0666)) == -1)
-			{
-				(errno == 2) ? ft_error_print(MSHELL, REDIR->filename, NULL, ERR1)
-					: ft_error_print(MSHELL, REDIR->filename,  NULL, ERR5);
-				data->redir_pipe_flag = 1;//to enter in ft_pipe_eof in main.c
-				return (1);
-			}
-			data->rd_file_name = REDIR->filename;
 		}
 		data->redirect_list = data->redirect_list->next;
 	}
-	close(file);
 	if (command->next && data->rd_file_name)
 		ft_redirect_read_pipe(data);
 	else
