5bac89f14274e4e8544b8c786d67b80ff70335f2
Rinat <nuretdin1405@gmail.com>
2021-01-13 18:30:31 +0300

    norm ft_pipe.c

--- functional/ft_pipe.c
+++ functional/ft_pipe.c
@@ -1,16 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pipe.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jheat <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/13 18:29:08 by jheat             #+#    #+#             */
+/*   Updated: 2021/01/13 18:29:11 by jheat            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "processing.h"
 
-void 	ft_pipe(t_data *data)
+void		ft_pipe(t_data *data)
 {
-	int			fd[2];
 	pid_t		pid;
-	int 		status = 0;
+	int			fd[2];
+	int			status;
 
+	status = 0;
 	pipe(fd);
 	pid = fork();
 	if (pid == -1)
 		ft_error_stderr(strerror(errno), 15);
-	if (pid == 0)//TODO ==
+	if (pid == 0)
 	{
 		dup2(fd[1], 1);
 		close(fd[0]);
