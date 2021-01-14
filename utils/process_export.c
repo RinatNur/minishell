2e78658b229fb03fb859b5978a4818cdd0c5c42b
wrudy <wrudy@student.21-school.ru>
2021-01-14 16:38:46 +0300

    some changes

--- utils/process_export.c
+++ utils/process_export.c
@@ -2,63 +2,16 @@
 #include "utils.h"
 #include "functional/processing.h"
 
-char **process_export_arg(char *export_arg)
+static void *exit_error1(char **str2)
 {
-	char **result;
-	int       equal_char_index;
+	ft_error_print(MSHELL, "export", NULL, "not a valid identifier");
+	free(str2);
+	return NULL;
+}
+
+static char **exit_error2(char **result, char *export_arg)
+{
 	int i;
-	char *mask;
-
-
-	if (!(result = malloc(sizeof(char *) * 3)))
-		exit(EXIT_FAILURE);
-	equal_char_index = find_char(export_arg, '=');
-	mask = get_mask(export_arg);
-	if (ft_isdigit(export_arg[0]))
-	{
-		ft_error_print(MSHELL, "export", NULL, "not a valid identifier");
-		free(mask);
-		free(result);
-		return NULL;
-	}
-	if (equal_char_index == 0)
-	{
-		ft_error_print(MSHELL, "export", NULL, "not a valid identifier");
-		free(mask);
-		free(result);
-		return NULL;
-	}
-
-	if (equal_char_index != -1)
-	{
-		if (export_arg[equal_char_index + 1] == '\0')
-		{
-			export_arg[equal_char_index] = '\0';
-			result[0] = u_strdup(export_arg);
-			result[1] = u_strdup("\0");
-			result[2] = NULL;
-		}
-		else
-		{
-			free(result);
-			free(mask);
-			mask = empty_str('0', u_strlen(export_arg));
-			i = equal_char_index + 1;
-			while (mask[i] != '\0')
-			{
-				mask[i] = '1';
-				i++;
-			}
-			result = u_split(export_arg, '=', mask);
-		}
-	}
-	else
-	{
-		result[0] = u_strdup("0");
-		result[1] = u_strdup(export_arg);
-		result[2] = NULL;
-	}
-	free(mask);
 
 	i = 0;
 	while (result[0][i] != '\0')
@@ -67,30 +20,72 @@ char **process_export_arg(char *export_arg)
 		{
 			ft_error_print(MSHELL, "export", NULL, "not a valid identifier");
 			free_2d_array(result);
-			return NULL;
+			return (NULL);
 		}
 		i++;
 	}
-
 	return (result);
 }
-char **process_export(char **export_with_arguments)
+
+void env_with_value(char ***result, const char *export_arg, int equal_char_index)
+{
+	char *mask;
+	int i;
+
+	free(*result);
+	mask = empty_str('0', u_strlen(export_arg));
+	i = equal_char_index + 1;
+	while (mask[i] != '\0')
+	{
+		mask[i] = '1';
+		i++;
+	}
+	*result = u_split(export_arg, '=', mask);
+	free(mask);
+}
+
+void env_without_value(char **result, char *export_arg, int equal_char_index)
+{
+	export_arg[equal_char_index] = '\0';
+	result[0] = u_strdup(export_arg);
+	result[1] = u_strdup("\0");
+	result[2] = NULL;
+}
+
+char **process_export_arg(char *export_arg)
+{
+	char **result;
+	int       equal_char_index;
+
+	if (!(result = malloc(sizeof(char *) * 3)))
+		exit(EXIT_FAILURE);
+	equal_char_index = find_char(export_arg, '=');
+	if (ft_isdigit(export_arg[0]))
+		return (exit_error1(result));
+	if (equal_char_index == 0)
+		return (exit_error1(result));
+	if (equal_char_index != -1)
+	{
+		if (export_arg[equal_char_index + 1] == '\0')
+			env_without_value(result, export_arg, equal_char_index);
+		else
+			env_with_value(&result, export_arg, equal_char_index);
+	}
+	else
+	{
+		result[0] = u_strdup("0");
+		result[1] = u_strdup(export_arg);
+		result[2] = NULL;
+	}
+	return (exit_error2(result, export_arg));
+}
+
+void set_export_arg(char **export_with_arguments, char **result)
 {
 	int i;
-	char **result;
-	char **tmp;
 	int j;
-	i = 0;
-	while (export_with_arguments[i] != NULL)
-		i++;
-	if (!(result = malloc(sizeof(char *) * (i * 2) )))
-		exit(EXIT_FAILURE);
-	result[0] = u_strdup(export_with_arguments[0]);
-	if (i == 1)
-	{
-		result[1] = NULL;
-		return (result);
-	}
+	char **tmp;
+
 	i = 1;
 	j = 1;
 	while (export_with_arguments[j] != NULL)
@@ -108,5 +103,24 @@ char **process_export(char **export_with_arguments)
 		j++;
 	}
 	result[i] = NULL;
+}
+
+char **process_export(char **export_with_arguments)
+{
+	int i;
+	char **result;
+
+	i = 0;
+	while (export_with_arguments[i] != NULL)
+		i++;
+	if (!(result = malloc(sizeof(char *) * (i * 2))))
+		exit(EXIT_FAILURE);
+	result[0] = u_strdup(export_with_arguments[0]);
+	if (i == 1)
+	{
+		result[1] = NULL;
+		return (result);
+	}
+	set_export_arg(export_with_arguments, result);
 	return (result);
 }
