1d6aca3752d6c1e0ccd2de3d13b0015dcee0f64d
Rinat <nuretdin1405@gmail.com>
2021-01-14 14:21:15 +0300

    norm ft_echo.c

--- functional/ft_echo.c
+++ functional/ft_echo.c
@@ -1,15 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_echo.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jheat <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/14 14:18:47 by jheat             #+#    #+#             */
+/*   Updated: 2021/01/14 14:18:49 by jheat            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "processing.h"
 
-void    ft_echo(t_data *data)
+static void		print_args(t_data *data, int i)
 {
-    int     i;
-    int 	j;
-    int     flag;
+	int		flag;
 
-    i = 1;
-    flag = 0;
-    while (data->ar[i])
-    {
+	flag = (i > 1) ? 1 : 0;
+	while (data->ar[i])
+	{
+		ft_write(1, data->ar[i]);
+		if (data->ar[i + 1])
+			ft_write(1, " ");
+		i++;
+	}
+	if (!flag)
+		ft_write(1, "\n");
+}
+
+static void		init(int *i, int *flag)
+{
+	*i = 1;
+	*flag = 0;
+}
+
+void			ft_echo(t_data *data)
+{
+	int		flag;
+	int		i;
+	int		j;
+
+	init(&i, &flag);
+	while (data->ar[i])
+	{
 		j = 0;
 		if (data->ar[i][j] == '-')
 		{
@@ -18,26 +51,14 @@ void    ft_echo(t_data *data)
 			{
 				if (data->ar[i][j] == 'n')
 					j++;
-				else
-				{
-					flag++;
-					break;
-				}
+				else if ((++flag))
+					break ;
 			}
 		}
 		if (!flag && j)
 			i++;
 		else
-			break;
+			break ;
 	}
-	flag = (i > 1) ? 1 : 0;
-    while (data->ar[i])
-	{
-       	ft_write(1, data->ar[i]);
-        if (data->ar[i + 1])
-            ft_write(1, " ");
-        i++;
-	}
-    if (!flag)
-        ft_write(1, "\n");
+	print_args(data, i);
 }
